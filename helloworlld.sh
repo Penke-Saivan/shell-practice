aa978f2143d584ea99003b70240216543b273ca9
SaiPavan-Penke <pavantonystark99@gmail.com>
2025-09-25 15:43:32 +0530

    Variables without space

--- helloworlld.sh
+++ helloworlld.sh
@@ -8,8 +8,8 @@ echo "Ramesh:: What are you learning recently?"
 echo "Suresh:: I am learning Shell scripting Ramesh"
 
 #Variables
-Person1= "Ramesh"
-Person2= "Suresh"
+Person1="Ramesh"
+Person2="Suresh"
 echo "$Person1:: Hi $Person2, How are you?"
 echo "$Person2:: Hello $Person1, I am fine. How are you?"
 echo "$Person1:: What are you learning recently? $Person2"
