b601513096445920082381826adb9c4be06f6d38
SaiPavan-Penke <pavantonystark99@gmail.com>
2025-09-25 15:39:06 +0530

    variables

--- helloworlld.sh
+++ helloworlld.sh
@@ -6,3 +6,11 @@ echo "Ramesh:: Hi Suresh, How are you?"
 echo "Suresh:: Hello Ramesh, I am fine. How are you?"
 echo "Ramesh:: What are you learning recently?"
 echo "Suresh:: I am learning Shell scripting Ramesh"
+
+#Variables
+Person1= "Ramesh"
+Person2= "Suresh"
+echo "$Person1:: Hi $Person2, How are you?"
+echo "$Person2:: Hello $Person1, I am fine. How are you?"
+echo "$Person1:: What are you learning recently? $Person2"
+echo "$Person2:: I am learning Shell scripting $Person1"
