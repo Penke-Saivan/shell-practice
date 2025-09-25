86a04c10b937e69cafa01d0eb949aa2124642283
SaiPavan-Penke <pavantonystark99@gmail.com>
2025-09-25 15:54:27 +0530

    Reading input from cmd and storing in the variable

--- passingArgs.sh
+++ passingArgs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#passing args to variables through command line"
 echo "$1 says Hello "
 echo "$2 says Hello back to $1 "
 echo "There by the conversation starts between $1 and $2"
+
---
+++ readingInput.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Reading input from cmd and storing in the variable
+echo "Please enter your name!"
+read -s Name
+echo "Confirm your name is $Name"
+
+
