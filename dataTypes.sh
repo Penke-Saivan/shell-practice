5cabbbff9d44d41ad4afec9abdead1da52093822
SaiPavan-Penke <pavantonystark99@gmail.com>
2025-09-25 22:08:27 +0530

    Arrays-Data Type

--- dataTypes.sh
+++ dataTypes.sh
@@ -4,5 +4,16 @@ SUM=$((NUM1+NUM2))
 echo "Sum is ${SUM}"
 NAME="DevOps"
 SUM1=$((NUM1+NAME))
-echo $SUM1
+echo "sum1 is $SUM1"
+echo $NAME
+echo "---------------"
+NAME=DevOps123
+echo $NAME
+
+echo "Arrays--------"
+Array=("Linux", "Shell", "Docker")
+echo "All : ${Array[@]}"
+echo "All : ${Array[2]}"
+echo "All : ${Array[1]}"
+echo "End- Arrays"
 
