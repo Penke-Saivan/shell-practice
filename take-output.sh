ada2fc0bf0955fe001e06b8115d5d1eb4e0c826c
SaiPavan-Penke <pavantonystark99@gmail.com>
2025-09-25 16:41:02 +0530

    made changes to take-output

--- take-output.sh
+++ take-output.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 Date=$date
-echo $Date
+echo "$Date"
 StartTime=$(date +s%)
 
-sleep 20
+sleep 10
 
 EndTime= $(date +s%)
 
-TotalTime= $((StartTime-EndTime))
+TotalTime= $(($StartTime-$EndTime))
 
 echo "Time elapsed is $TotalTime"
