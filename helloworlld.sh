8abd42e8d15b569476be595b685d5de7771ccff7
SaiPavan-Penke <pavantonystark99@gmail.com>
2025-09-25 15:35:10 +0530

    Conversation

--- helloworlld.sh
+++ helloworlld.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hello Worlld"
+# Conversation
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh, I am fine. How are you?"
+echo "Ramesh:: What are you learning recently?"
+echo "Suresh:: I am learning Shell scripting Ramesh"
